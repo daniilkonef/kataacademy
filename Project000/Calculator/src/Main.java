21d171213ba9e94a634c15d6e9630660fb0f317c
daniilkonef <daniilkonef@yandex.ru>
2023-01-28 15:44:17 +0300

    Создал словарь greekAndDec

--- Project000/Calculator/src/Main.java
+++ Project000/Calculator/src/Main.java
@@ -65,26 +65,54 @@ public class Main {
 
 
 
-        // псевдокод греческого вычислителя
-        // массивСтрок греческиеРазрешенныеЧисла = { массив разрешенных греческих цифр } HashMap<Integer, String> passportsAndNames = new HashMap<>();
-        HashMap<Integer, String> decAndGreek = new HashMap<>();
-        decAndGreek.put(1,"I");
-        decAndGreek.put(2,"II");
-        decAndGreek.put(3,"III");
-        decAndGreek.put(4,"IV");
-        decAndGreek.put(5,"V");
-        decAndGreek.put(6,"VI");
-        decAndGreek.put(7,"VII");
-        decAndGreek.put(8,"VIII");
-        decAndGreek.put(9,"IX");
-        decAndGreek.put(10,"X");
+//         псевдокод греческого вычислителя
+//         Словарь греческиеРазрешенныеЧисла = { массив разрешенных греческих цифр } HashMap<Integer, String> passportsAndNames = new HashMap<>(); https://javarush.com/groups/posts/1940-klass-hashmap-
+//         истинность леваяЧастьГодна? = проверить что левая часть входит в Словарь разрешенных символов и вернуть истину или ложь
+//         истинность праваяЧастьГодна? = проверить правую часть входит в Словарь разрешенных символов и вернуть истину или ложь
+//         если (обе части имеют истину) то:
+//              преобразовать левую часть в десятичное число;
+//              преобразовать правую часть в десятичное число;
 
-        // истинность леваяЧастьГодна? = проверить что левая входит в массив разрешенных символов и вернуть истину или ложь
-        // истинность праваяЧастьГодна? = проверить правую часть на соответствие разрешенному символу и вернуть истину или ложь
-        // если (обе части имеет истину) то:
-        //      преобразовать левую часть в десятичное число;
-        //      преобразовать правую часть в десятичное число;
-        //
+//        HashMap<Integer, String> decAndGreek = new HashMap<>();
+//        decAndGreek.put(1,"I");
+//        decAndGreek.put(2,"II");
+//        decAndGreek.put(3,"III");
+//        decAndGreek.put(4,"IV");
+//        decAndGreek.put(5,"V");
+//        decAndGreek.put(6,"VI");
+//        decAndGreek.put(7,"VII");
+//        decAndGreek.put(8,"VIII");
+//        decAndGreek.put(9,"IX");
+//        decAndGreek.put(10,"X");
+//        System.out.println(decAndGreek);
+//        System.out.println(decAndGreek.get(7));
+
+        HashMap<String,Integer> greekAndDec = new HashMap<>();
+        greekAndDec.put("I",1);
+        greekAndDec.put("II",2);
+        greekAndDec.put("III",3);
+        greekAndDec.put("IV",4);
+        greekAndDec.put("V",5);
+        greekAndDec.put("VI",6);
+        greekAndDec.put("VII",7);
+        greekAndDec.put("VIII",8);
+        greekAndDec.put("IX",9);
+        greekAndDec.put("X",10);
+        System.out.println(greekAndDec);
+        System.out.println(greekAndDec.get("VII"));
+        System.out.println(greekAndDec.containsKey("VII"));
+
+        String s1=null, s2=null;
+        try {
+            s1 = parts[0];  ob1 = s1;
+            s2 = parts[0];  ob2 = s2;
+
+
+        }
+        catch (Exception e)
+        {
+            //System.out.print("Ваше выражение имеет смешанные системы исчисления,");
+        }
 
         return result;
     }
